d98f5d2a624f2e8ca2244a4ef226330b9fef35ea
colbyfayock <fayock@gmail.com>
2020-10-03 16:31:46 -0400

    fixing the request chain, updating how the environment variables are pulled in, updating README

--- src/discord.js
+++ src/discord.js
@@ -1,4 +1,7 @@
+require('dotenv').config();
+
 const axios = require("axios");
+
 const today = new Date();
 
 const openings = [
@@ -6,7 +9,7 @@ const openings = [
   "Greetings, Earthlings!",
   "Pens at the ready!",
   "Hope you're ready for some inking!",
-  "Beep! Boop! Time to ink!",
+  "Beep! Boop! Time to ink!"
 ];
 
 const topics = {
@@ -48,14 +51,15 @@ const finishings = [
   "Draw on inkers!",
   "The journey is as important as the destination.",
   "There is no competition - show us what you made.",
+  "1... 2... 3... Ink!"
 ];
 
 function constructMessage() {
-  return `${selectRandomFrom(
-    openings
-  )} It is the ${getDate()} of Inktober and today's theme is ${
-    topics[today.getDate()]
-  }. ${selectRandomFrom(finishings)}`;
+  const opening = selectRandomFrom(openings);
+  const date = getDate();
+  const theme = topics[today.getDate()];
+  const closing = selectRandomFrom(finishings);
+  return `${opening} It is the ${date} of Inktober and today's theme is **${theme}**. ${closing}`;
 }
 
 function selectRandomFrom(selection) {
@@ -92,12 +96,12 @@ const params = {
   content: constructMessage(),
 };
 
-function triggerWebhook() {
+async function triggerWebhook() {
   return axios.post(process.env.DISCORD_WEB_HOOK, params);
 }
 
-exports.handler = async (event, context, callback) => {
-  return triggerWebhook()
+exports.handler = (event, context, callback) => {
+  triggerWebhook()
     .then(() => {
       callback(null, {
         statusCode: 200,
